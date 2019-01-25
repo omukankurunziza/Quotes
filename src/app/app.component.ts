d862910c0a014f76665b3ee2e8213dfe2efe18c8
mukankunziza opportunee <nshutioppo@yahoo.fr>
2019-01-25 14:42:59 +0200

    add list of Quotes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+ 
 })
 export class AppComponent {
-  title = 'Quotes';
+  // quote='Dreaming quotes'
+  quote: string[];
+  //  quote='Dreaming quotes'
+
+  constructor(){
+
+    this.quote = ["Every great dream begins with a dreamer. Always remember, you have within you the strength, the patience, and the passion to reach for the stars to change the world","A dream doesn't become reality through magic; it takes sweat, determination and hard work","All our dreams can come true, if we have the courage to pursue them"];
+  }
 }
