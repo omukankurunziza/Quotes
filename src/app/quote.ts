63625fb0a6d615186128a20a5f7570e35d84258f
mukankunziza opportunee <nshutioppo@yahoo.fr>
2019-01-30 11:43:18 +0200

    add deployment

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -2,7 +2,7 @@ export class Quote {
     
     
     public showDescription:boolean;
-    constructor(public id: number,public name: string, public description:string,public completeDate:Date){
+    constructor(public id: number,public name: string, public description:string,public completeDate:Date,public author:string,public submitted:string){
    
         this.showDescription=false
         
